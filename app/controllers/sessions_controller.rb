43f96df54b0ec51e0e65bec5a7a02e90e367d2cb
Abe Yoshinao <yoshi.abe4470@gmail.com>
2019-08-25 12:31:40 +0000

    #5-advanced_login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,22 +2,20 @@ class SessionsController < ApplicationController
   def new
   end
   
-  def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      log_in user
-      redirect_to user
+   def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
-      # エラーメッセージを作成する
-      flash.now[:danger] = 'password or address wrong'
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
-  end
-  
+   end
+
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
-
 end
