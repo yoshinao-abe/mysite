e101aeae9b241a03ec9c298b16c4febefb9ee1a2
Abe Yoshinao <yoshi.abe4470@gmail.com>
2019-09-02 04:24:50 +0000

    #6.5-AccountActivations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,24 @@ class SessionsController < ApplicationController
   def new
   end
   
-   def create
+  def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message = "アカウントは有効ではありません"
+        message += "eメールからアカウントの登録を完了させてください！"
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
-   end
+  end
 
   def destroy
     log_out if logged_in?
