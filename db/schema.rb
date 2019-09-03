10af1f6d5ae05daaa9800242029d828f56b64a07
Abe Yoshinao <yoshi.abe4470@gmail.com>
2019-09-03 15:29:30 +0000

    #6-password_reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_01_120448) do
+ActiveRecord::Schema.define(version: 2019_09_02_053609) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2019_09_01_120448) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
