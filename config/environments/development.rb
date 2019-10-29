7d253c32691ac5c62b034d55f9d0b39d606717c8
Abe Yoshinao <yoshi.abe4470@gmail.com>
2019-10-29 09:21:31 +0000

    deploy#3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-
+  config.web_console.development_only = false
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
