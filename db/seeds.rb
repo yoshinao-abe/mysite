49267fc62bca071592d73e302a839e43c6d274ac
Abe Yoshinao <yoshi.abe4470@gmail.com>
2019-11-08 10:52:34 +0000

    seed add tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,12 +16,26 @@ User.create!(name:  "Example User",
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
-               
 end
+
 users = User.order(:created_at).take(6)
-50.times do
+
+25.times do
   content = Faker::Lorem.sentence(word_count: 5)
-  users.each { |user| user.microposts.create!(content: content) }
+  tag = "ドラゴンクエスト"
+  users.each { |user| user.microposts.create!(content: content, tag_list: tag) }
+end
+
+15.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  tag = "ポケモン"
+  users.each { |user| user.microposts.create!(content: content, tag_list: tag) }
+end
+
+10.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  tag = "モンスターハンター"
+  users.each { |user| user.microposts.create!(content: content, tag_list: tag) }
 end
 
 #リレーションシップ
